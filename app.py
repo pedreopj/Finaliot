b9d3fbc4ec64abb2a82be1dc4a766ac6b77d0a89
pedreopj <pelaezescobarpepo@gmail.com>
2025-05-08 20:26:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ Este panel presenta en tiempo real las condiciones de temperatura, humedad y rad
 # Sustituye esta URL por el link público de tu dashboard de Grafana (public snapshot o embed)
 grafana_url = "https://pelaezescobarpepo.grafana.net/dashboard/snapshot/GJz1cXQWPsbofx5ekEKSdpj1hNEi0EKA"
 
-components.iframe("grafana_url" , height=800, scrolling=True)
+components.iframe("https://pelaezescobarpepo.grafana.net/dashboard/snapshot/wqETKzwr91ZKmSpulKCbBBjaCCVHWyZH" , height=800, scrolling=True)
